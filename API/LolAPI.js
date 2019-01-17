53d4207aa3c050c9a53c92b5cc46dd14cfce93ec
julien-guillermet <45063806+julien-guillermet@users.noreply.github.com>
2019-01-17 15:01:35 +0100

    api v3 to v4
    api v3 to v4

--- API/LolAPI.js
+++ API/LolAPI.js
@@ -2,7 +2,7 @@
 const API_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 
 export function getSummonerBySummonerName(name, server) {
-    const url = "https://" + server + ".api.riotgames.com/lol/summoner/v3/summoners/by-name/" + name + "?api_key=" + API_TOKEN
+    const url = "https://" + server + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + API_TOKEN
     return fetch(url)
         .then((response) => response.json())
         .catch((error) => console.error(error))
@@ -10,14 +10,14 @@ export function getSummonerBySummonerName(name, server) {
 
 
 export function getLeageBySummonerId(summonerId, server) {
-    const url = "https://" + server + ".api.riotgames.com/lol/league/v3/positions/by-summoner/" + summonerId + "?api_key=" + API_TOKEN
+    const url = "https://" + server + ".api.riotgames.com/lol/league/v4/positions/by-summoner/" + summonerId + "?api_key=" + API_TOKEN
     return fetch(url)
         .then((response) => response.json())
         .catch((error) => console.error(error))
 }
 
 export function getMatchsByAccountId(accountId, server, index) {
-    const url = "https://" + server + ".api.riotgames.com/lol/match/v3/matchlists/by-account/" + accountId + "?endIndex=" + index + "&api_key=" + API_TOKEN
+    const url = "https://" + server + ".api.riotgames.com/lol/match/v4/matchlists/by-account/" + accountId + "?endIndex=" + index + "&api_key=" + API_TOKEN
     return fetch(url)
         .then((response) => response.json())
         .catch((error) => console.error(error))
@@ -25,7 +25,7 @@ export function getMatchsByAccountId(accountId, server, index) {
 
 
 export function getInfoMatchByMatchId(matchId, server) {
-    const url = "https://" + server + ".api.riotgames.com/lol/match/v3/matches/" + matchId + "?api_key=" + API_TOKEN
+    const url = "https://" + server + ".api.riotgames.com/lol/match/v4/matches/" + matchId + "?api_key=" + API_TOKEN
     return fetch(url)
         .then((response) => response.json())
         .catch((error) => console.error(error))
